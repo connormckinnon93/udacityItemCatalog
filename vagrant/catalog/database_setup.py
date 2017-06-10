7894b3928c88a489f52854e9d9bea50ddbe374b0
Connor McKinnon <mckinnondconnor@gmail.com>
2017-06-10 10:19:15 -0400

    add sqlalchemy database setup with sqlite

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+ 
+class User(Base):
+    __tablename__ = 'user'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+ 
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+class Item(Base):
+	__tablename__ = 'item'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250), nullable=False)
+	description = Column(String(250), nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+
+	@property
+	def return_cat_name(self):
+		return self.category.return_cat_name
+
+	@property
+	def return_user_name(self):
+		return self.user.return_user_name
+
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+        	'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category': self.return_cat_name,
+            'user': self.return_user_name
+        }
+ 
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
